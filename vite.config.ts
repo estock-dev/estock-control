665a4d2216323b0d3bb924213ae43cd282f7ccc1
Raphael Palmer <palmer@Raphaels-Mac-Studio.local>
2024-04-16 01:17:41 -0300

    structure fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import styleImport from 'vite-plugin-style-import';
-import { createStyleImportPlugin } from 'vite-plugin-style-import';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createStyleImportPlugin({
-
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        modifyVars: {
-          'hack': `true; @import (reference) "${require.resolve('antd/lib/style/color/colorPalette.less')}";`,
-          ...require('antd/dist/theme').default, // Ensure you include the default theme first
-          ...require('antd/dist/theme').dark, // Apply dark theme variables
-        },
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
